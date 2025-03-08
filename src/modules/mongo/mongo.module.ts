cd57143c4690c11ca55879065cd62b8d9afbd3ff
yoodz <904201050@qq.com>
2025-03-08 22:18:46 +0800

    2

--- src/modules/mongo/mongo.module.ts
+++ src/modules/mongo/mongo.module.ts
@@ -6,6 +6,7 @@ import { MongoDBService } from './mongo.service';
     providers: [
         {
             provide: MongoDBService,
+            useValue: new MongoDBService('mongostring', 'blog-news'),
         },
     ],
     exports: [MongoDBService], // 导出服务
